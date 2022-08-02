98cce0aef23be1fc3a6e04f53de50e61c89a4518
*Matheus <*thebravematteo33@protonmail.com*>
2022-08-02 10:11:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Importação tradicional com chaves. 
+// Para especificar o que está importando 
+import { Post } from './Post' 
+
+export function App() {
+
+  return (
+    <div>
+        <Post />
+     
+    </div>
+  )
+}
+
