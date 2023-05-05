4678684b565b42e19df668a53576d6c50602a5fc
matteothebrave <79720034+matteothebrave@users.noreply.github.com>
2023-05-05 16:40:27 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ export function Sidebar() {
       src="https://images.unsplash.com/photo-1437603568260-1950d3ca6eab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50" />
 
       <div className={styles.profile}>
-        <Avatar src="https://media-exp1.licdn.com/dms/image/C4D03AQFtIew6ZsLCJw/profile-displayphoto-shrink_800_800/0/1659371939155?e=1665619200&v=beta&t=lsC4aoOThrr_QFEMrbHUutyeXSV77V3ht0Lp7eX9lcA" />
+        <Avatar src="https://avatars.githubusercontent.com/u/79720034?v=4" />
         <strong>Matheus Grossi</strong>
         <span>Ministério de missões</span>
       </div>
